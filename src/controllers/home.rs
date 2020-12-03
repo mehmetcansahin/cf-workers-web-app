f2ca59bf4522632307d895fe81d0087d69a256df
mehmetcansahin <sahinmcan@gmail.com>
2020-12-04 01:34:28 +0300

    update webworker

--- src/controllers/home.rs
+++ src/controllers/home.rs
@@ -1,22 +1,10 @@
-use crate::utils::{json_response, Params, ResponseData};
+use serde_json::json;
 use web_sys::{Request, Response};
+use webworker::{response_json, Params};
 
 pub fn index(_request: Request, _params: Params) -> Response {
-    let response_data = ResponseData {
-        message: "Hello, World!".to_string(),
-        ..Default::default()
-    };
-    json_response(response_data, 200)
-}
-
-pub fn hello_user(_request: Request, params: Params) -> Response {
-    let hello_user = format!(
-        "Hello, {}!",
-        params.iter().find(|&x| x.0 == "username").unwrap().1
-    );
-    let response_data = ResponseData {
-        message: hello_user,
-        ..Default::default()
-    };
-    json_response(response_data, 200)
+    let data = json!({
+        "message": "Hello, World!".to_string()
+    });
+    response_json(data, None)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 extern crate cfg_if;
 extern crate wasm_bindgen;
 
-#[macro_use]
-pub mod macros;
-pub mod controllers;
-mod routes;
-pub mod utils;
-
+use crate::controllers::home::index;
 use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 use web_sys::{ExtendableEvent, Request, Response};
+use webworker::WebWorker;
+
+mod controllers;
 
 cfg_if! {
     // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
@@ -21,6 +19,8 @@ cfg_if! {
 }
 
 #[wasm_bindgen]
-pub fn handle_event(_event: ExtendableEvent, request: Request) -> Response {
-    routes::run(request)
+pub fn handle_event(event: ExtendableEvent, request: Request) -> Response {
+    let mut ww = WebWorker::default();
+    ww.route("/GET/".to_string(), Box::new(index));
+    ww.handle(event, request)
 }
